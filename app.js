127439776ae38ffc32d4ec4350064b35c2007b45
= <=>
2020-10-20 10:26:48 +0100

    Added cors module

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const db = require('./models/index')
 
 const bodyParser = require('body-parser');
 
-
+const cors = require('cors');
 
 
 const app = express();
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
 
 
 
+app.use(cors());
 
 app.use('/', routes);
 
